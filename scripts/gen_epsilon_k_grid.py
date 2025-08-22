1dcf4def1ad1163cef55ba0296c88684d3671eac
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-08-22 20:29:36 +0800

    python script to generate k-grid manually for epsilon calculation

---
+++ scripts/gen_epsilon_k_grid.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+def print_points(kx: int, ky: int, kz: int, precision: int = 8) -> None:
+    fmt = f"{{:.{precision}e}} {{:.{precision}e}} {{:.{precision}e}} {{:.{precision}e}}"
+    print(kx * ky * kz)
+    for i, j, k in product(range(kx), range(ky), range(kz)):
+        print(fmt.format(i / kx, j / ky, k / kz, 1 / (kx * ky * kz)))
+
+if __name__ == "__main__":
+    print_points(6, 6, 6, precision=8)
