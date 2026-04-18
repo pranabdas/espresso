f0e4543f66460dd90b9a66fe35c45e75766919d6
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-04-18 12:07:50 +0800

    chore: bump deps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -181,7 +181,7 @@ const config = {
 
   stylesheets: [
     {
-      href: "https://pranab.pages.dev/assets/fonts/katex/0.16.28/katex.min.css",
+      href: "https://pranab.pages.dev/assets/fonts/katex/0.16.45/katex.min.css",
       type: "text/css",
     },
   ],
